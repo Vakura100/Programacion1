b189dfc0e9a4074097befd4266b67c10bcf2b9de
Vakura100 <guillermo.ortiz@uniat.edu.mx>
2023-11-30 10:55:30 -0600

    Aseguradora

--- Progra1/cmake/Examen.cpp
+++ Progra1/cmake/Examen.cpp
@@ -1,3 +1,16 @@
-int main(){
+#include <string>
+#include <iostream>
 
-}
+class Insurances {
+public:
+     Insurances (std::string name, std::string description, int price, int coverage,int id,std::string curp, std::string birthDate) {
+         this->name = name;
+         this->description = description;
+         this->price = price;
+         this->coverage = coverage;
+         this->id = id;
+         this->curp = curp;
+         this->birthDate = birthDate
+
+
+     };
